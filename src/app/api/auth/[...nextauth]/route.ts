530fe228acfa28a395dd0a433aa904f1a8b3a6dd
bit2swaz <bit2swaz@gmail.com>
2025-08-09 02:51:24 +0530

    in the process of adding github oauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,29 @@
-import { handlers } from "~/server/auth";
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
 
-export const { GET, POST } = handlers;
+import { db } from "~/server/db";
+import { env } from "~/env";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHubProvider({
+      clientId: env.AUTH_GITHUB_ID,
+      clientSecret: env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+});
+
+export { handler as GET, handler as POST };
