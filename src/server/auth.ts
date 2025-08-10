1aadde150816fbe71fb2db767f9595178042b4ca
bit2swaz <bit2swaz@gmail.com>
2025-08-10 23:20:33 +0530

    create central auth config file

---
+++ src/server/auth.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GitHub from "next-auth/providers/github";
+import { type NextAuthConfig } from "next-auth";
+
+import { db } from "~/server/db";
+import { env } from "~/env";
+
+export const authConfig: NextAuthConfig = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: env.AUTH_GITHUB_ID,
+      clientSecret: env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  pages: {
+    error: "/auth/error",
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
