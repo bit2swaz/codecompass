407a2a12f74d6e81130061cdf09c3e8279b9cb16
bit2swaz <bit2swaz@gmail.com>
2025-08-17 22:48:47 +0530

    register diagnostic router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -11,14 +11,6 @@ export const appRouter = createTRPCRouter({
   diag: diagRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
 
-/**
- * Create a server-side caller for the tRPC API.
- * @example
- * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
- * ^? Post[]
- */
 export const createCaller = createCallerFactory(appRouter);
